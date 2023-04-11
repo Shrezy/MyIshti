65f2d4f41bccd859553f574e11f504fcf2e1343d
shrezy <shreyasi.sri@gmail.com>
2023-04-12 01:42:01 +0530

    changes

--- database/db.js
+++ database/db.js
@@ -1,10 +1,8 @@
 import mongoose from 'mongoose';
 
-const Connection = async (username, password) => {
-    console.log(username);
-    console.log(password);
-    const URL = `mongodb://${username}:${password}@ac-oz16yzn-shard-00-00.qntbcc3.mongodb.net:27017,ac-oz16yzn-shard-00-01.qntbcc3.mongodb.net:27017,ac-oz16yzn-shard-00-02.qntbcc3.mongodb.net:27017/?ssl=true&replicaSet=atlas-ieoo99-shard-0&authSource=admin&retryWrites=true&w=majority`
+const Connection = async (URL) => {
     try {
+        
         await mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
         console.log('Database connected successfully');
     } catch (error) {
--- index.js
+++ index.js
@@ -18,13 +18,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', Router);
 
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
 console.log('Hello world');
-console.log(password);
 
-Connection(username, password);
+const URL = process.env.MONGODB_URI || `mongodb://${username}:${password}@ac-oz16yzn-shard-00-00.qntbcc3.mongodb.net:27017,ac-oz16yzn-shard-00-01.qntbcc3.mongodb.net:27017,ac-oz16yzn-shard-00-02.qntbcc3.mongodb.net:27017/?ssl=true&replicaSet=atlas-ieoo99-shard-0&authSource=admin&retryWrites=true&w=majority`
+Connection(URL);
 
 app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
